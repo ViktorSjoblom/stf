8033705c973956f3b75eef414a346ac088548038
Viktor Sjöblom <Viktorsjoblom93@gmail.com>
2020-08-20 15:44:13 +0200

    final changes hopefully x2

--- app.js
+++
@@ -1,6 +0,0 @@
-const arrow = document.getElementById("drop-down-arrow");
-const dropDownMenu = document.getElementById("dropdown-menu");
-
-arrow.addEventListener("click", function () {
-  dropDownMenu.classList.toggle("flex");
-});
